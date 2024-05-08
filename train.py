0d1c629ae5cc3d4a5f0bf8ea2b2c07dd3926a0bd
Paul Eisenhuth <eisenhuth@informatik.uni-leipzig.de>
2024-05-08 17:20:19 +0200

    Turned trained into a hyperparameter callable script

--- train.py
+++ train.py
@@ -122,77 +122,186 @@ def train(num_epochs, eval_every_n_epochs, dataloader_train, dataloader_eval, in
                 if save_weights:
                     torch.save(interaction_model.state_dict(), "model_weights/model_" + str(e) + ".weights")
 
+    return eval_loss
 
-def main():
+def create_model(config):
+    acts = {
+        "ReLU" : nn.ReLU(), 
+        "Sigmoid" : nn.Sigmoid(), 
+        "Hardtanh" : nn.Hardtanh()
+    }
+
+    inter_pred = InteractionPredictor(
+            # node embedding mlp
+            node_emb_hidden_layers = [config['node_emb_hidden_layers']] if config['node_emb_hidden_layers'] != 0 else [],
+            node_embedding_size = config['node_embedding_size'],
+
+            # message weights mlp
+            msg_weights_hidden_layers = [config['msg_weights_hidden_layers']] if config['msg_weights_hidden_layers'] != 0 else [],
+            msg_weights_act = acts[config['weights_act']],
+
+            # message tp spherical harmonics edge
+            pattern_spherical_harmonics_l = config['spherical_harmonics_l'],
+
+            # message format
+            irreps_message_scalars = config['irreps_message_scalars'], 
+            irreps_message_vectors = config['irreps_message_vectors'], 
+            irreps_message_tensors = config['irreps_message_tensors'],
+
+            # message batch normalization
+            batch_normalize_msg = config['batch_normalize_msg'],
+
+            # node update weights mlp
+            node_update_hidden_layers = [config['node_update_hidden_layers']] if config['node_update_hidden_layers'] != 0 else [],
+            node_update_act = acts[config['weights_act']],
+
+            # geometric node format
+            irreps_node_scalars = config['irreps_node_scalars'], 
+            irreps_node_vectors = config['irreps_node_vectors'], 
+            irreps_node_tensors = config['irreps_node_tensors'],
+
+            # node update batch normalization
+            batch_normalize_update=config['batch_normalize_update'],
+
+            # interaction tp weights mlp
+            basis_density_per_A = config['basis_density_per_A'],
+            inter_tp_weights_hidden_layers = [config['inter_tp_weights_hidden_layers']] if config['inter_tp_weights_hidden_layers'] != 0 else [],
+            inter_tp_weights_act = acts[config['weights_act']],
+
+            # interaction tp spherical harmonics
+            inter_spherical_harmonics_l = config['spherical_harmonics_l'],
+
+            # general
+            n_pattern_layers = config['n_pattern_layers'],
+            radius = config['radius']
+        )
+    
+    return inter_pred
+
+def main(
+        config,
+        epochs,
+        abs_path,
+        data_dir,
+        split_file,
+        storage_path
+):
     from prepare_pdbbind import pdb_ignore_list
     from prepare_pdbbind import defined_interactions
 
-    df = pd.read_csv('LP_PDBBind.csv', index_col=0)
+    df = pd.read_csv(abs_path + split_file, index_col=0)
     train_pdbs = [ x for x in list(df[(df['new_split'] == 'train') & df.CL1 & ~df.covalent].index) if x not in pdb_ignore_list ]
     test_pdbs = [ x for x in list(df[(df['new_split'] == 'test') & df.CL1 & ~df.covalent].index) if x not in pdb_ignore_list ]
     val_pdbs = [ x for x in list(df[(df['new_split'] == 'val') & df.CL1 & ~df.covalent].index) if x not in pdb_ignore_list ]
 
+    validation_loss = 0.0
+
     for interaction_type in defined_interactions:
         print("@@@@ ", interaction_type)
-        inter_pred = InteractionPredictor(
-            # node embedding mlp
-            node_emb_hidden_layers = [],
-            node_embedding_size = 8,
-
-            # message weights mlp
-            msg_weights_hidden_layers = [],
-            msg_weights_act = torch.relu,
-
-            # message tp spherical harmonics edge
-            pattern_spherical_harmonics_l = 2,
-
-            # message format
-            irreps_message_scalars = 8, 
-            irreps_message_vectors = 1, 
-            irreps_message_tensors = 1,
-
-            # message batch normalization
-            batch_normalize_msg = True,
-
-            # node update weights mlp
-            node_update_hidden_layers = [],
-            node_update_act = torch.relu,
-
-            # geometric node format
-            irreps_node_scalars = 8, 
-            irreps_node_vectors = 1, 
-            irreps_node_tensors = 1,
-
-            # node update batch normalization
-            batch_normalize_update=False,
-
-            # interaction tp weights mlp
-            basis_density_per_A = 5,
-            inter_tp_weights_hidden_layers = [],
-            inter_tp_weights_act = torch.relu,
-
-            # interaction tp spherical harmonics
-            inter_spherical_harmonics_l = 2,
-
-            # general
-            n_pattern_layers = 3,
-            radius = 7.5
-        )
+        inter_pred = create_model(config=config)
 
         print("Model weights:", sum(p.numel() for p in inter_pred.parameters() if p.requires_grad))
 
         loss_fn = nn.BCEWithLogitsLoss()
         optimizer = torch.optim.Adam(inter_pred.parameters(), lr=1e-3, amsgrad=True)
 
-        dataset_train = PDBBindInteractionDataset("pdbbind2020/", train_pdbs, interaction_type)
+        dataset_train = PDBBindInteractionDataset(abs_path + data_dir, train_pdbs, interaction_type)
         dataloader_train = DataLoader(dataset_train, batch_size=64, shuffle=True, collate_fn=dataset_train.collate_fn, pin_memory=True, num_workers=10)
-        dataset_test = PDBBindInteractionDataset("pdbbind2020/", test_pdbs, interaction_type)
-        dataloader_test = DataLoader(dataset_test, batch_size=64, shuffle=True, collate_fn=dataset_test.collate_fn, pin_memory=True, num_workers=10)
+        dataset_val = PDBBindInteractionDataset(abs_path + data_dir, val_pdbs, interaction_type)
+        dataloader_val = DataLoader(dataset_val, batch_size=64, shuffle=True, collate_fn=dataset_val.collate_fn, pin_memory=True, num_workers=10)
 
-        print("Training size:", len(dataset_train), "Test size:", len(dataset_test))
+        print("Training size:", len(dataset_train), "Validation size:", len(dataset_val))
 
-        train(20, 10, dataloader_train, dataloader_test, inter_pred, loss_fn, optimizer, save_weights=False)
+        validation_loss += train(epochs, 50, dataloader_train, dataloader_val, inter_pred, loss_fn, optimizer, save_weights=False) ** 2
         print("\n-------------------------------------------------------------\n")
 
+    validation_loss = validation_loss ** 0.5
+    print(f"RESULT: {validation_loss:>8f} \n")
+
 if __name__ == "__main__":
-    main()
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Trains a Kernel to predict biomolecular interactions')
+    parser.add_argument('--epochs', type=int, required=True)
+    
+    parser.add_argument('--node_emb_hidden_layers', type=int, required=True)
+    parser.add_argument('--node_embedding_size', type=int, required=True)
+
+    parser.add_argument('--msg_weights_hidden_layers', type=int, required=True)
+    parser.add_argument('--weights_act', required=True)
+
+    parser.add_argument('--spherical_harmonics_l', type=int, required=True)
+
+    parser.add_argument('--irreps_message_scalars', type=int, required=True)
+    parser.add_argument('--irreps_message_vectors', type=int, required=True)
+    parser.add_argument('--irreps_message_tensors', type=int, required=True)
+
+    parser.add_argument('--batch_normalize_msg', type=bool, required=True)
+
+    parser.add_argument('--node_update_hidden_layers', type=int, required=True)
+
+    parser.add_argument('--irreps_node_scalars', type=int, required=True)
+    parser.add_argument('--irreps_node_vectors', type=int, required=True)
+    parser.add_argument('--irreps_node_tensors', type=int, required=True)
+
+    parser.add_argument('--batch_normalize_update', type=bool, required=True)
+
+    parser.add_argument('--basis_density_per_A', type=int, required=True)
+    parser.add_argument('--inter_tp_weights_hidden_layers', type=int, required=True)
+    parser.add_argument('--radius', type=float, required=True)
+
+    parser.add_argument('--n_pattern_layers', type=int, required=True)
+    parser.add_argument('--batch_size', type=int, required=True)
+    parser.add_argument('--lr', type=float, required=True)
+
+    parser.add_argument('--abs_path', required=True,
+                        help='Absolute path for all in and out files')
+    parser.add_argument('--data_dir', required=True,
+                        help='Path to pdbbind within the project path')
+    parser.add_argument('--split_file', required=True,
+                        help='Path to LP_PDBBind split file within the project path')
+    parser.add_argument('--storage_path', required=True,
+                        help='Path to directory for saving results within the project path')
+    
+    args = parser.parse_args()
+
+    config = {
+        "node_emb_hidden_layers": args.node_emb_hidden_layers,
+        "node_embedding_size": args.node_embedding_size,
+
+        "msg_weights_hidden_layers": args.msg_weights_hidden_layers,
+        "weights_act": args.weights_act,
+
+        "spherical_harmonics_l": args.spherical_harmonics_l,
+
+        "irreps_message_scalars": args.irreps_message_scalars,
+        "irreps_message_vectors": args.irreps_message_vectors,
+        "irreps_message_tensors": args.irreps_message_tensors,
+
+        "batch_normalize_msg": args.batch_normalize_msg,
+
+        "node_update_hidden_layers": args.node_update_hidden_layers,
+
+        "irreps_node_scalars": args.irreps_node_scalars,
+        "irreps_node_vectors": args.irreps_node_vectors,
+        "irreps_node_tensors": args.irreps_node_tensors,
+
+        "batch_normalize_update": args.batch_normalize_update,
+
+        "basis_density_per_A": args.basis_density_per_A,
+        "inter_tp_weights_hidden_layers": args.inter_tp_weights_hidden_layers,
+        "radius": args.radius,
+
+        "n_pattern_layers": args.n_pattern_layers,
+        "batch_size": args.batch_size,
+        "lr": args.lr,
+    }
+
+    main(
+        config=config,
+        epochs=args.epochs,
+        abs_path=args.abs_path,
+        data_dir=args.data_dir,
+        split_file=args.split_file,
+        storage_path=args.storage_path
+    )
